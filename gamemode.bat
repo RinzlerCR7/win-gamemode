0c7fe85fbbc0c02511c27b7f8036fe4fc4e3f9f4
Rinzler <57419788+RinzlerCR7@users.noreply.github.com>
2024-12-24 10:42:53 +0530

    Add files via upload

--- gamemode.bat
+++ gamemode.bat
@@ -2,13 +2,15 @@
 
 title gamemode.bat
 
+set path=%~dp0
+
 :: Stopping unnecessary services
-for /f "tokens=1,2 delims=," %%a in (services.csv) do (
+for /f "tokens=1,2 delims=," %%a in (%path%services.csv) do (
 	if "%%b"=="Y" net stop "%%a"
 )
 
 :: Killing unnecessary processes
-for /f "tokens=1,2 delims=," %%a in (processes.csv) do (
+for /f "tokens=1,2 delims=," %%a in (%path%processes.csv) do (
 	if "%%b"=="Y" taskkill /f /im "%%a"
 )
 
